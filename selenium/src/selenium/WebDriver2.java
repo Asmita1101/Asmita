54c3d493d1b4a5213b79e67b494e434537619d02
HP <HP@DESKTOP-RA9L680>
2023-06-18 12:10:31 +0530

    commit

---
+++ selenium/src/selenium/WebDriver2.java
@@ -0,0 +1,10 @@
+package selenium;
+
+public class WebDriver2 {
+
+	public static void main(String[] args) {
+System.out.println("hii");
+
+	}
+
+}
---
+++ selenium/src/selenium/Webdriver1.java
@@ -0,0 +1,16 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+public class Webdriver1 {
+
+	public static void main(String[] args) {
+	System.setProperty("Webdriver.chrome.driver","./chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com");
+	driver.findElement(By.tagName("input"));
+	System.out.println("element identified");
+
+}
+}
